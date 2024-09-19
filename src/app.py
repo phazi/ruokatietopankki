9f5cd5d738ae272103681271697783538e0f5cd7
Pasi <50963794+phazi@users.noreply.github.com>
2024-09-19 19:24:45 +0300

    Add connection to db and test functioning

--- src/app.py
+++ src/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres"
+db = SQLAlchemy(app)
+
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return "Tervetuloa Ruokatietopankkiin!"
+
+@app.route("/page/<int:id>")
+def page(id):
+    sql = text("SELECT foodname FROM food WHERE foodid = (:id)")
+    result = db.session.execute(sql,{"id":id})
+    food_name = result.fetchone()
+    return "Tämä on sivu " + str(id) + " eli " + food_name.foodname
+
+@app.route("/test")
+def test():
+    content = ""
+    for i in range(100):
+        content += str(i + 1) + " "
+    return content
